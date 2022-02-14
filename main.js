123d27d82a9b7ae7810e168977651631282c0260
Debangi-Dey <74201092+Debangi-Dey@users.noreply.github.com>
2022-02-14 16:53:09 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -6,9 +6,17 @@ video.size(350, 350)
 posenet=ml5.poseNet(video,modelLoaded)
 posenet.on('pose', gotPoses)
 }
-
+NoseX=0
+NoseY=0
+lwristX=0
+rwristX=0
+difference=0
 function draw(){
     background("pink")
+    document.getElementById("square_sides").innerHTML="The width & height of a square will be "+difference+"px"
+    fill("aqua")
+    stroke("dark blue")
+    square(NoseX, NoseY,difference)
 }
 
 function modelLoaded(){
@@ -18,5 +26,10 @@ function modelLoaded(){
 function gotPoses(results){
 if (results.length>0) {
     console.log(results)
+    NoseX=results[0].pose.nose.x
+    NoseY=results[0].pose.nose.y
+    lwristX=results[0].pose.leftWrist.x
+    rwristX=results[0].pose.rightWrist.x
+    differnce=floor(lwristX-rwristX)
 }
 }
